88e81a3128a629cfc040c76e3636f0bc6f170afb
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2022-09-28 14:23:54 -0700

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useStateContext } from '../context/StateContext'
+
+import Link from 'next/link'
+import { AiOutlineShopping } from 'react-icons/ai'
+import {Cart} from './index'
+const Navbar = () => {
+  const {showCart, setShowCart, totalQuantities}=useStateContext()
+  return (
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">
+          JoshFreeman Store
+        </Link>
+      </p>
+      <button type='button' className='cart-icon' onClick={()=>setShowCart((prevState) => !prevState)
+}>
+        <AiOutlineShopping/>
+        <span className='cart-item-qty'>{totalQuantities}</span>
+      </button>
+
+    {showCart && <Cart/>}
+    </div>
+  )
+}
+
+export default Navbar
